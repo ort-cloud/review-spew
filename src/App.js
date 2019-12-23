4691bb8ad6c7525b326386ddebc23f34405d1c86
Carl Ortutay <carl.orty@gmail.com>
2019-12-23 13:02:13 -0500

    added nav to landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Landing from "./Landing";
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div>
+      <Landing />
     </div>
   );
 }
---
+++ src/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function Landing() {
+  return (
+    <div className='App'>
+      <nav>Nav
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+}
+
+export default Landing;
