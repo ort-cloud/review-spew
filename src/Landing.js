ae69cea6fd55762baedde3f5879c48ecd2549341
Carl Ortutay <carlorty@gmail.com>
2020-03-06 15:40:57 -0500

    Added description statement

--- src/Landing.js
+++ src/Landing.js
@@ -10,12 +10,11 @@ class Landing extends Component {
         <main>
           <header>
             <h1>Review Spew</h1>
-            <h2>[tag line here]</h2>
+            <p>Stuck trying to figure out what to watch? Maybe you’re a movie nerd and you’re not sure if the dubbed German toture-horror, or the sci fi/thriller/space noir is right for your blog? Maybe it’s late, you just got that text to chill, and now you have to choose between the ultra-violent anime you were going to watch or literally anything else? This is where Review Spew comes in to hold your hair back for you. Enter the name of the movie or tv show in question and Review Spew will regurgitate reviews from the top review sites from all across the web. A fair and honest upchuck of all the pretentious opinions your cinephilic heart desires smeared across your screen.</p>
           </header>
 
           <section>
-            <h3>Check it out!</h3>
-            <p>[TODO: make search bar]</p>
+            <h2>Check it out!</h2>
           </section>
 
           <section>
