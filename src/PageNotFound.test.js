2601273392897882fb0b29e426614a6d04f357f6
Carl Ortutay <carlorty@gmail.com>
2020-03-15 13:32:23 -0400

    PageNotFound test passing

---
+++ src/PageNotFound.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+import PageNotFound from './PageNotFound';
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <BrowserRouter>
+      <PageNotFound />
+    </BrowserRouter>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
